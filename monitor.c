1910b2884e5f64f47dd9fc5a30d5bd36bdac991a
zamfi <zamfirescu.stfan12@gmail.com>
2021-01-04 11:25:39 +0200

    Add reading from terminal Removing useless mutex The structure for input is: Size buffer Number of Producer Array of values for producer Number of Consumer Array of values for consumer

--- monitor.c
+++ monitor.c
@@ -5,21 +5,21 @@
 #include<semaphore.h>
 #include <unistd.h>
 #include <errno.h>
-sem_t mutexShow;
+
 struct Monitor {
     int sizeBuffer;
     sem_t mutex, full, empty;
 }m;
 
-int semWait(sem_t* sem){
-    if(sem_wait(sem)){
+inline int semWait(sem_t* sem) {
+    if(sem_wait(sem)) {
         perror(NULL);
         return errno;
     }
 }
 
-int semPost(sem_t* sem){
-    if(sem_post(sem)){
+inline int semPost(sem_t* sem) {
+    if(sem_post(sem)) {
         perror(NULL);
         return errno;
     }
@@ -33,12 +33,10 @@ void* producer (void* param) {
         semWait(&m.mutex);
 
         if (*numItems > 0 ){
-            semWait(&mutexShow);
             printf("%d added\n", 1);
-            semPost(&mutexShow);
             (*numItems)--;
-
         }
+
         if (*numItems == 0) {
             semPost(&m.empty);
             semPost(&m.mutex);
@@ -58,11 +56,10 @@ void* consumer (void* param) {
         semWait(&m.mutex);
 
         if (*numItems > 0) {
-            semWait(&mutexShow);
             printf("%d taken\n", 1);
-            semPost(&mutexShow);
             (*numItems) --;
         }
+        
         if (*numItems == 0) {
             semPost(&m.full);
             semPost(&m.mutex);
@@ -73,7 +70,7 @@ void* consumer (void* param) {
         semPost(&m.mutex);
     }
 }
-int init(struct Monitor* m, int pSizeBuff){
+int init(struct Monitor* m, int pSizeBuff) {
     
     m->sizeBuffer = pSizeBuff;
     if (sem_init(&m->mutex, 0, 1) || sem_init(&m->full, 0, pSizeBuff) || sem_init(&m->empty, 0, 0)) {
@@ -83,26 +80,35 @@ int init(struct Monitor* m, int pSizeBuff){
     return 0;
 }
 int main (int argc, char*argv[]) {
+    // freopen("test1.txt", "r", stdin);
 
-    if (argc < 3) {
-        printf("Not enough parameters\n");
+    int sizeBuff, numOfProducer, numOfConsumer;
+    scanf("%d%d", &sizeBuff, &numOfProducer);
+    if (sizeBuff == 0 || numOfProducer == 0) {
+        printf("Wrong input\n");
         return 0;
     }
-    int sizeBuff = atoi(argv[1]);
-    int numOfProducer = atoi(argv[2]);
-    int numOfConsumer = atoi(argv[3]);
+
+    int prod[numOfProducer];
+    for (int i = 0; i < numOfProducer; i++) {
+        scanf("%d", &prod[i]);
+    }
+
+    scanf("%d", &numOfConsumer);
+    if(numOfConsumer == 0) {
+        printf("Wrong input\n");
+        return 0;
+    }
+    int cons[numOfConsumer];
+    for( int i = 0; i < numOfConsumer; i++) {
+        scanf("%d", &cons[i]);
+    }
 
     init (&m, sizeBuff);
     pthread_t ptid[numOfProducer];
     pthread_t ctid[numOfConsumer];
-    if (sem_init(&mutexShow, 0, 1)) {
-        perror(NULL);
-        return errno;
-    }   
-    int prod[] = {2, 1};
-    int cons[] = {3};
-   
-    for (int i = 0; i < numOfConsumer; i++){
+
+    for (int i = 0; i < numOfConsumer; i++) {
         int *x = (int*)malloc(sizeof(int));
         *x = cons[i];
         if (pthread_create(&ctid[i], NULL, consumer, (void*)(x))) {
@@ -110,7 +116,7 @@ int main (int argc, char*argv[]) {
             return errno;
         }
     }
-    for (int i = 0; i < numOfProducer; i++){
+    for (int i = 0; i < numOfProducer; i++) {
         int *x = (int*)malloc(sizeof(int));
         *x = prod[i];
         if (pthread_create(&ptid[i], NULL, producer, (void*)(x))){
@@ -119,11 +125,11 @@ int main (int argc, char*argv[]) {
         }
     }
 
-    for(int i = 0; i < numOfProducer;i++){
+    for(int i = 0; i < numOfProducer;i++) {
         pthread_join(ptid[i], NULL);
     }
 
-    for(int i = 0; i < numOfConsumer;i++){
+    for(int i = 0; i < numOfConsumer;i++) {
         pthread_join(ctid[i], NULL);
     }
     
